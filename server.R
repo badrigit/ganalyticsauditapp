ba7e14b48a4f91823d83d4b08bba6e35360c7cc2
Badri Srinivasan <badri.it@gmail.com>
2014-12-24 15:07:20 +0000

    phantomjs runs and quits for each url

--- server.R
+++ server.R
@@ -15,11 +15,12 @@ shinyServer(function(input, output) {
   
   # Get network log for each url
   getNetworkLog <- function(urls){
-    pJS <- phantom()
-    Sys.sleep(5) # give the binary a moment
-    remDr <- remoteDriver(browserName = "phantom")
-    remDr$open()
-    result <- remDr$phantomExecute("var page = this;
+    for(i in 1: nrow(urls)){
+      pJS <- phantom()
+      Sys.sleep(5) # give the binary a moment
+      remDr <- remoteDriver(browserName = "phantom")
+      remDr$open()
+      result <- remDr$phantomExecute("var page = this;
                                     page.onResourceRequested = function (req) {
                                       if (req.url.indexOf('collect?') > -1) {
                                         var collect = req.url;
@@ -27,11 +28,9 @@ shinyServer(function(input, output) {
                                       }
                                     }
                                   ")
-    for(i in 1:nrow(urls)){
       remDr$navigate(urls[i,1])
-      Sys.sleep(5)
+      pJS$stop()
     }
-    pJS$stop()
   }
   
   output$urlsTable <- renderTable({
