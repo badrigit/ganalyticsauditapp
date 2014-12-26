2860637c868a4393f2d2ef8ad7d51585a0b76507
Badri Srinivasan <badri.it@gmail.com>
2014-12-26 19:09:25 +0000

    moved functions to seperate script

---
+++ www/global.R
@@ -0,0 +1,49 @@
+# Parse XML file
+getSitemapUrls <- function(sitemapURL){
+  sitemap <- htmlTreeParse(sitemapURL, useInternalNodes = T)
+  urls <- data.frame(URL=xpathSApply(sitemap, "//url/loc", xmlValue))
+  return(urls)
+}
+
+# Get Analytics URL parameters
+getAnalyticsUrlParam <- function(analyticsUrl){
+  parseUrl <- parse_url(analyticsUrl)
+  params <- cbind(parseUrl$query$dl, parseUrl$query$t, parseUrl$query$tid)
+  return(params)
+}
+
+# Get network log for each url
+getNetworkLog <- function(urls){
+  for(i in 1:nrow(urls)){
+    prog
+    pJS <- phantom()
+    Sys.sleep(2) # give the binary a moment
+    remDr <- remoteDriver(browserName = "phantom")
+    remDr$open()
+    result <- remDr$phantomExecute("var page = this;
+                                      var fs = require('fs');
+                                      page.onResourceRequested = function (req) {
+                                        if (req.url.indexOf('analytics.com/collect?') > -1||
+                                            req.url.indexOf('analytics.com/r/collect?') > -1) {
+                                          var collect = req.url;
+                                          console.log(collect);
+                                          console.log(page.url);
+
+                                          try{
+                                            fs.write('output.txt',collect+'\\n','a',function(err){
+                                              if(err) {
+                                                console.log(err);
+                                              } else {
+                                                  console.log('The file was saved!');
+                                              }
+                                            });
+                                          } catch(e){
+                                            console.log(e);
+                                          }
+                                        }
+                                      }
+                                    ")
+    remDr$navigate(urls[i,1])
+    pJS$stop()
+  }
+}
