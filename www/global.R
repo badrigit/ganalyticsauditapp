8be317026ec3e34ae10cfc9bfa8714cf66cdda9f
Badri Srinivasan <badri.it@gmail.com>
2014-12-29 08:35:14 +0000

    added progress bar

--- www/global.R
+++ www/global.R
@@ -14,36 +14,41 @@ getAnalyticsUrlParam <- function(analyticsUrl){
 
 # Get network log for each url
 getNetworkLog <- function(urls){
-  for(i in 1:nrow(urls)){
-    prog
+  withProgress(message = paste("Total Number of URLs: ",nrow(urls),sep = ""),
+               min = 1, 
+               max = nrow(urls), expr = for(i in 1:nrow(urls)){
     pJS <- phantom()
     Sys.sleep(2) # give the binary a moment
     remDr <- remoteDriver(browserName = "phantom")
     remDr$open()
     result <- remDr$phantomExecute("var page = this;
-                                      var fs = require('fs');
-                                      page.onResourceRequested = function (req) {
-                                        if (req.url.indexOf('analytics.com/collect?') > -1||
-                                            req.url.indexOf('analytics.com/r/collect?') > -1) {
-                                          var collect = req.url;
-                                          console.log(collect);
-                                          console.log(page.url);
-
-                                          try{
-                                            fs.write('output.txt',collect+'\\n','a',function(err){
-                                              if(err) {
-                                                console.log(err);
-                                              } else {
-                                                  console.log('The file was saved!');
-                                              }
-                                            });
-                                          } catch(e){
-                                            console.log(e);
-                                          }
-                                        }
-                                      }
-                                    ")
+                                     var fs = require('fs');
+                                     page.onResourceRequested = function (req) {
+                                     if (req.url.indexOf('analytics.com/collect?') > -1||
+                                     req.url.indexOf('analytics.com/r/collect?') > -1) {
+                                     var collect = req.url;
+                                     console.log(collect);
+                                     console.log(page.url);
+                                     
+                                     try{
+                                     fs.write('output.txt',collect+'\\n','a',function(err){
+                                     if(err) {
+                                     console.log(err);
+                                     } else {
+                                     console.log('The file was saved!');
+                                     }
+                                     });
+                                     } catch(e){
+                                     console.log(e);
+                                     }
+                                     }
+                                     }
+                                     ")
+      
     remDr$navigate(urls[i,1])
+      
+    incProgress(amount = 1/nrow(urls), message = paste("Remaining Urls: ", nrow(urls) - i, sep=""))
     pJS$stop()
   }
+  )
 }
