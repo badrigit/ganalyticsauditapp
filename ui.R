5b0b4b6a4ecce7e7ec8b50e81a87f7d2798100b3
Badri Srinivasan <badri.it@gmail.com>
2015-01-20 18:00:40 +0000

    upgraded UI

--- ui.R
+++ ui.R
@@ -1,48 +1,56 @@
-library(shiny)
-library(shinythemes)
+library(shinydashboard)
 
-shinyUI(
-  fluidPage(theme = shinytheme("flatly"),
-    
-    titlePanel(list(HTML("<img src='logo.png'/>"), "Analytics Audit"),
-               windowTitle = "Web Analytics Audit"),
-    
-    sidebarLayout(
-      
-      sidebarPanel(
-        
-        radioButtons(inputId = "inputType",
-                     label = "InputType", 
-                     choices = c("Sitemap URL" = "sitemapURL", "File Upload" = "fileUpload")),
-        conditionalPanel(
-          condition = "input.inputType == 'sitemapURL'",
-          textInput(inputId = "sitemap", label = "Sitemap URL")
-        ),
-        conditionalPanel(
-          condition = "input.inputType == 'fileUpload'",
-          fileInput('file', 'Choose file to upload',
-                    accept = c(
-                      'text/csv',
-                      'text/comma-separated-values',
-                      'text/tab-separated-values',
-                      'text/plain',
-                      '.csv',
-                      '.xml'
-                    )
-          )
-        ),
-        br(),
-        actionButton(inputId = "submit", label = "Submit")
-      ),
-      
-      mainPanel(
-        tabsetPanel(
-          id = "tab",
-          tabPanel("Dashboard",htmlOutput(outputId = "dashboardOutput")),
-          tabPanel("Google Tag Manager",dataTableOutput(outputId = "gtmOuput")),
-          tabPanel("Google Analytics",dataTableOutput(outputId = "analyticsOuput"))
-        )
-      )
-    )
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Analytics Audit Tool", tabName = "analyticsAudit", icon = icon("cog")),
+    menuItem("Help", tabName = "help", icon = icon("th"),badgeLabel= "new", badgeColor = "green")
   )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "analyticsAudit",
+            fluidRow(
+              box(
+                title = "Input Section",
+                radioButtons(inputId = "inputType",
+                             label = "InputType", 
+                             choices = c("Sitemap URL" = "sitemapURL", "File Upload" = "fileUpload")),
+                conditionalPanel(
+                  condition = "input.inputType == 'sitemapURL'",
+                  textInput(inputId = "sitemap", label = "Sitemap URL")
+                ),
+                conditionalPanel(
+                  condition = "input.inputType == 'fileUpload'",
+                  fileInput('file', 'Choose file to upload',
+                            accept = c(
+                              'text/csv',
+                              'text/comma-separated-values',
+                              'text/tab-separated-values',
+                              'text/plain',
+                              '.csv',
+                              '.xml'
+                            )
+                  )
+                ),
+                br(),
+                actionButton(inputId = "submit", label = "Submit")),
+              box(
+                tabsetPanel(
+                  id = "tab",
+                  tabPanel("Dashboard",htmlOutput(outputId = "dashboardOutput")),
+                  tabPanel("Google Tag Manager",dataTableOutput(outputId = "gtmOuput")),
+                  tabPanel("Google Analytics",dataTableOutput(outputId = "analyticsOuput"))
+                )
+              )
+            )
+    ),
+    tabItem(tabName = "help", h2("Help Section"))
+  )
+)
+
+ui <- dashboardPage(skin = "blue",
+                    dashboardHeader(title = "Analytics Audit"),
+                    sidebar,
+                    body
 )
